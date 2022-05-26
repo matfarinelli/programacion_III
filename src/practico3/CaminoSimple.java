c89d44af47b56a8df1712d84f2947bc8f010f007
Matias Farinelli <matfarinelli@gmail.com>
2022-05-25 21:18:40 -0300

    CaminoMasLargo - practico 4.3 no brinda solucion

--- src/practico3/CaminoSimple.java
+++ src/practico3/CaminoSimple.java
@@ -6,10 +6,8 @@ import java.util.Iterator;
 
 // Ejercicio 4.
 // Escribir un algoritmo que, dado un grafo dirigido y dos vértices i, j de este
-// grafo, devuelva el
-// camino simple (sin ciclos) de mayor longitud del vértice i al vértice j.
-// Puede suponerse que
-// el grafo de entrada es acíclico.
+// grafo, devuelva el camino simple (sin ciclos) de mayor longitud del vértice i al vértice j.
+// Puede suponerse que el grafo de entrada es acíclico.
 
 public class CaminoSimple {
 
@@ -40,13 +38,17 @@ public class CaminoSimple {
         if (visitados.get(origen) == false) {
             camino.addAll(bfs_visit(origen, destino));
         }
+
         return camino;
     }
 
     public ArrayList<Integer> bfs_visit(int v_origen, int destino) {
+        ArrayList<Integer> tramo_actual = new ArrayList<Integer>();
+        boolean destino_encontrado = false;
+
+        camino.add(v_origen);
         visitados.put(v_origen, true);
         filaVertices.add(v_origen);
-        boolean destino_encontrado = false;
 
         while (!filaVertices.isEmpty() && !destino_encontrado) {
             int vertice_top = filaVertices.remove(0);
@@ -54,21 +56,23 @@ public class CaminoSimple {
 
             while (itAdyacentes.hasNext()) {
                 int vertice_ady = itAdyacentes.next();
+                tramo_actual.add(vertice_ady);
 
                 if (visitados.get(vertice_ady) == false) {
                     visitados.put(vertice_ady, true);
                     filaVertices.add(vertice_ady);
 
                     if (vertice_ady == destino) {
+                        // camino.addAll(tramo_actual);
+                        // System.out.println(camino);
                         System.out.println("Destino encontrado!!!");
-                    } else
-                        System.out.println("Camino no encontrado");
+                    }
                 }
 
             }
         }
 
-        return null;
+        return camino;
     }
 
 }
